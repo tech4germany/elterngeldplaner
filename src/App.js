cd55779881d90d44d4a4601d16bd1937222f8a86
jenny <jenny.huang@campus.lmu.de>
2022-10-20 15:38:58 +0200

    added alert dialog when user is on desktop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
 import { FormikContext } from 'formik';
-import { useState, createContext, useContext, useEffect } from 'react';
+import { useState, createContext, useContext, useEffect, useRef } from 'react';
+import {
+  AlertDialog,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogCloseButton,
+  Button,
+  useDisclosure
+} from '@chakra-ui/react';
 import Planner from './pages/planner/Planner';
 import Header from './components/ui/Header';
 import NameInputPage from './pages/nameInputPage/NameInputPage';
@@ -10,10 +21,14 @@ import BirthDatePage from './pages/birthDatePage/BirthDatePage';
 import IncomePage from './pages/incomePage/IncomePage';
 import EGOverviewPage from './pages/egOverview/EGOverviewPage';
 import FinalSummeryPage from './pages/finalSummeryPage/FinalSummeryPage';
+import { TextNormal } from './components/styled/StyledText';
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState(<StartPage />);
   const { activeStepIndex, setActiveStepIndex, formData, setFormData } = useContext(FormContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef();
+
   const pageNames = [
     'Start',
     'Wie heißt ihr?',
@@ -23,6 +38,45 @@ const App = () => {
     'Planer',
     'Zusammenfassung eurer Planung'
   ];
+
+  const desktopAlertDialog = (
+    <AlertDialog
+      motionPreset="slideInBottom"
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}
+      isOpen={isOpen}
+      isCentered>
+      <AlertDialogOverlay />
+
+      <AlertDialogContent>
+        <AlertDialogHeader>Das ist eine mobile Anwendung</AlertDialogHeader>
+        <AlertDialogCloseButton />
+        <AlertDialogBody>
+          Dieser Prototyp wurde bisher nur für Mobilgeräte umgesetzt. <br /> Bitte besuche die Seite
+          auf deinem Mobilgerät für die beste User Experience :)
+        </AlertDialogBody>
+        <AlertDialogFooter>
+          <Button colorScheme="red" ml={3} ref={cancelRef} onClick={onClose}>
+            Okay
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+
+  const handleResize = () => {
+    if (window.innerWidth > 768) {
+      // user is on desktop
+      onOpen();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize());
+    return () => {
+      window.removeEventListener('resize', handleResize());
+    };
+  }, []);
 
   useEffect(() => {
     switch (activeStepIndex) {
@@ -55,9 +109,8 @@ const App = () => {
 
   return (
     <>
+      {desktopAlertDialog}
       <Header pageNames={pageNames} />
-      {/* <Planer /> */}
-      {/* <IntroductionPage /> */}
       {currentPage}
     </>
   );
